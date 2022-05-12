f3f4e2905338680331887c2dd100e2026919e382
tar1971 <96209005+tar1971@users.noreply.github.com>
2022-05-12 20:41:47 +0200

    Update installer1.sh

--- installer1.sh
+++ installer1.sh
@@ -13,5 +13,16 @@ wait
 opkg install --force-overwrite https://github.com/tar1971/turkvod/blob/main/enigma2-plugin-extensions-turkvod_12.00_PY3.all.ipk?raw=true
 
 ###########################################
+wait
+#!/bin/sh
+#
+
+wget -O /tmp/dreamsatpanel_1.3.deb "https://github.com/tar1971/dreamsatpanel/blob/main/enigma2-plugin-extensions-dreamsatpanel_1.3.deb?raw=truee"
+wait
+apt-get update ; dpkg -i /tmp/*.deb ; apt-get -y -f install
+wait
+dpkg -i --force-overwrite /tmp/*.deb
+wait
+sleep 2;
 sleep 2;
 exit 0

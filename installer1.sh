a4d8603eb9e9529058942249642b4ecbcc0be546
tar1971 <96209005+tar1971@users.noreply.github.com>
2022-05-12 20:37:57 +0200

    Add files via upload

---
+++ installer1.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+######################################################################################
+## Command=wget https://raw.githubusercontent.com/tar1971/turkvod/main/installer1.sh -O - | /bin/sh
+##
+###########################################
+###########################################
+#!/bin/sh
+echo
+
+opkg install --force-overwrite https://github.com/tar1971/turkvod/blob/main/enigma2-plugin-extensions-turkvod_12.00_all.ipk?raw=true
+wait
+opkg install --force-overwrite https://github.com/tar1971/turkvod/blob/main/enigma2-plugin-extensions-turkvod_12.00_PY3.all.ipk?raw=true
+
+sleep 2;
+exit 0
